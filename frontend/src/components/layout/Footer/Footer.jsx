9965e64c67e82e0a844725fda460f65d48b890fc
matuteduro <matias_duro@hotmail.com>
2022-10-31 17:37:20 -0300

    changes

--- frontend/src/components/layout/Footer/Footer.jsx
+++ frontend/src/components/layout/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
     </div>
 
     <div className="midFooter">
-        <h1>DURODEV.</h1>
+        <h1>DURODEV</h1>
         <p>High Quality is our first priority</p>
         <p>Copyright 2022 &copy; Durodev</p>
     </div>
